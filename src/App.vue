682e264ee2c14c0bcb2f357c2dca505184ae0f21
rhickey94 <rhickey94@gmail.com>
2022-07-27 19:45:33 -0400

    accidentally finished the entire project in the first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import QRCode from "./components/QRCode.vue";
+</script>
+
+<template>
+  <QRCode />
+</template>
+
+<style lang="scss" scoped>
+main {
+  font-size: large;
+}
+</style>
---
+++ src/components/QRCode.vue
@@ -0,0 +1,47 @@
+<script setup></script>
+<template>
+  <section class="qr-card">
+    <div class="image-container">
+      <img src="../assets/image-qr-code.png" alt="weenus" />
+    </div>
+    <h2>Improve your front-end skills by building projects</h2>
+    <p>
+      Scan the QR code to visit Frontend Mentor and take your coding skills to
+      the next level
+    </p>
+  </section>
+</template>
+<style lang="scss" scoped>
+.qr-card {
+  padding: 16px;
+  max-width: 320px;
+  background-color: var(--vt-c-white);
+  border-radius: 20px;
+
+  h2 {
+    font: var(--vt-f-header);
+    color: var(--vt-c-dark-navy);
+    text-align: center;
+    margin-bottom: 16px;
+  }
+  p {
+    font: var(--vt-f-body);
+    color: var(--vt-c-gray);
+    text-align: center;
+    margin: 0 auto 24px;
+    width: 256px;
+  }
+}
+
+.image-container {
+  margin: 0 auto;
+  margin-bottom: 24px;
+  img {
+    border-radius: 10px;
+    width: auto;
+    height: auto;
+    max-width: 288px;
+    max-height: 288px;
+  }
+}
+</style>
